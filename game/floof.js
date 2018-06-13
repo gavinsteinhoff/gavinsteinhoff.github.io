4d901cfa573414df7ad48097d6561ceedb35ca40
gavinsteinhoff <gavinsteinhoff@gmail.com>
2018-06-13 14:08:18 -0500

    Add files via upload

---
+++ game/floof.js
@@ -0,0 +1,71 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var floofs = document.getElementsByClassName("floof");
+
+    var paragraphsMIN = 1;
+    var paragraphsMAX = 2;
+
+    Array.prototype.forEach.call(floofs, function (floof) {
+
+        if (floof.classList.contains("floof-lg")) {
+            paragraphsMIN = 3;
+            paragraphsMAX = 5;
+        }
+        if (floof.classList.contains("floof-md")) {
+            paragraphsMIN = 2;
+            paragraphsMAX = 3;
+        }
+        if (floof.classList.contains("floof-sm")) {
+            paragraphsMIN = 1;
+            paragraphsMAX = 1;
+        }
+        var paragraphs = getRnd(paragraphsMIN, paragraphsMAX);
+        var paragraph = "";
+
+        var link = -1;
+        var bold = -1;
+
+        for (var i = 0; i < paragraphs; i++) {
+            var sentences = getRnd(4, 8);
+            if (floof.classList.contains("floof-x")) {
+                link = getRnd(0, sentences);
+                bold = getRnd(0, sentences);
+            }
+
+            for (var o = 0; o < sentences; o++) {
+                var words = getRnd(5, 10);
+                var sentence = getWord(1).toUpperCase();
+
+                for (var j = 0; j < words; j++) {
+                    var char = getRnd(1, 7);
+                    switch (j) {
+                        case link:
+                            sentence += "<a href='#'>" + getWord(char) + '</a> ';
+                            break;
+                        case bold:
+                            sentence += "<b>" + getWord(char) + '</b> ';
+                            break;
+                        default:
+                            sentence += getWord(char) + ' ';
+                    }
+                }
+                sentence += getWord(5) + ". ";
+                paragraph += sentence;
+            }
+            paragraph += "<br><br>";
+        }
+        floof.innerHTML = paragraph;
+    });
+}, false);
+
+function getWord(char) {
+    var output = "";
+    var chars = "abcdefghijklmnopqrstuvwxyz";
+    for (var i = 0; i < char; i++) {
+        output += chars.charAt(getRnd(0, 26));
+    }
+    return output;
+}
+
+function getRnd(low, high) {
+    return Math.floor((Math.random() * high) + low);
+}
