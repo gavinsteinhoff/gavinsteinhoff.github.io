f282824904d9f3a77c4442b05f894af1b98e78c2
gavinsteinhoff <gavinsteinhoff@gmail.com>
2018-06-20 16:58:04 -0500

    Add files via upload

---
+++ santa/utils.js
@@ -0,0 +1,21 @@
+//start of .js file
+var game = {
+    id: null,
+    ref: 'games/',
+    name: null,
+    getRef: function () {
+        return this.ref + this.id;
+    }
+}
+
+function displayNames() {
+    database.ref(game.getRef() + '/names').on('value', (function (snapshot) {
+        names = snapshot.val();
+        output = document.getElementById('names');
+        output.innerHTML = "";
+        for (var key in names) {
+            output.innerHTML += key + "<br />";
+        }
+    }));
+}
+//end of .js file
